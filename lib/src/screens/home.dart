879b969a4554265b62a69c80e8e54e9a26212af3
patrickchow0803 <patrickchow0803@gmail.com>
2020-10-19 20:07:24 -0400

    Added a horizontal list view for the categories

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -73,6 +73,47 @@ class _HomeState extends State<Home> {
                   ),
                 ),
               ),
+            ),
+            SizedBox(height: 5.0),
+            Container(
+              height: 120,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 8,
+                itemBuilder: (_, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: red[50],
+                                offset: Offset(4, 6),
+                                blurRadius: 12,
+                              ),
+                            ],
+                          ),
+                          child: Padding(
+                              padding: EdgeInsets.all(6.0),
+                              child: Image.asset(
+                                'images/salad.png',
+                                width: 50,
+                              )),
+                        ),
+                        SizedBox(height: 10),
+                        CustomText(
+                          text: 'Salad',
+                          size: 14,
+                          colors: black,
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
             )
           ],
         ),
