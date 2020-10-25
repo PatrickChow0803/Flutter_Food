f4613fc75a49426139e819646b3fae8c35fd6a00
patrickchow0803 <patrickchow0803@gmail.com>
2020-10-25 13:00:20 -0400

    added a category model and refactored out the category widget to it's own widget

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_food/src/widgets/categories.dart';
 import 'package:flutter_food/src/widgets/custom_text.dart';
 import '../commons.dart';
 
@@ -75,46 +76,7 @@ class _HomeState extends State<Home> {
               ),
             ),
             SizedBox(height: 5.0),
-            Container(
-              height: 120,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 8,
-                itemBuilder: (_, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            color: white,
-                            boxShadow: [
-                              BoxShadow(
-                                color: red[50],
-                                offset: Offset(4, 6),
-                                blurRadius: 12,
-                              ),
-                            ],
-                          ),
-                          child: Padding(
-                              padding: EdgeInsets.all(6.0),
-                              child: Image.asset(
-                                'images/salad.png',
-                                width: 50,
-                              )),
-                        ),
-                        SizedBox(height: 10),
-                        CustomText(
-                          text: 'Salad',
-                          size: 14,
-                          colors: black,
-                        )
-                      ],
-                    ),
-                  );
-                },
-              ),
-            )
+            Categories(),
           ],
         ),
       ),
---
+++ lib/src/widgets/categories.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_food/src/models/category.dart';
+
+import '../commons.dart';
+import 'custom_text.dart';
+
+List<Category> categoriesList = [
+  Category(
+    name: "Salad",
+    image: "salad.png",
+  ),
+];
+
+class Categories extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categoriesList.length,
+        itemBuilder: (_, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: red[50],
+                        offset: Offset(4, 6),
+                        blurRadius: 12,
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                      padding: EdgeInsets.all(6.0),
+                      child: Image.asset(
+                        'images/${categoriesList[index].image}',
+                        width: 50,
+                      )),
+                ),
+                SizedBox(height: 10),
+                CustomText(
+                  text: categoriesList[index].name,
+                  size: 14,
+                  colors: black,
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
