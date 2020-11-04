807ffb60977affa390ff1a95c0ff425c2f726c5c
patrickchow0803 <patrickchow0803@gmail.com>
2020-11-04 16:29:18 -0500

    Added initialize and _onStateChanged methods to the auth file

--- lib/src/providers/auth.dart
+++ lib/src/providers/auth.dart
@@ -16,7 +16,27 @@ class AuthProvider with ChangeNotifier {
   Status _status = Status.Uninitialized;
   UserServices _userServices = UserServices();
   UserModel _userModel;
+  final formKey = GlobalKey<FormState>();
+  TextEditingController email = TextEditingController();
+  TextEditingController name = TextEditingController();
+  TextEditingController password = TextEditingController();
 
   Status get status => _status;
   Firebase.User get user => _user;
+
+  AuthProvider.initialize() : _auth = Firebase.FirebaseAuth.instance {
+    _auth.authStateChanges().listen(_onStateChanged);
+  }
+
+  Future<void> _onStateChanged(Firebase.User firebaseUser) async {
+    if (firebaseUser == null) {
+      _status = Status.Uninitialized;
+    } else {
+      _user = firebaseUser;
+      _status = Status.Authenticated;
+      _userModel = await _userServices.getUserById(firebaseUser.uid);
+    }
+
+    notifyListeners();
+  }
 }
