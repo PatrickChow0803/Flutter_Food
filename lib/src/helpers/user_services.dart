a6518c34ba1d1f07950db3241b12a6fa0c9bb3e2
patrickchow0803 <patrickchow0803@gmail.com>
2020-11-04 13:09:25 -0500

    Added user_service

---
+++ lib/src/helpers/user_services.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_food/src/models/user.dart';
+
+class UserServices {
+  String collection = 'users';
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  void createUser(Map<String, dynamic> values) {
+    _firestore.collection(collection).doc(values['id']).set(values);
+  }
+
+  void updateUserData(Map<String, dynamic> values) {
+    _firestore.collection(collection).doc(values['id']).update(values);
+  }
+
+  Future<UserModel> getUserById(String id) =>
+      _firestore.collection(collection).doc(id).get().then((doc) => UserModel.fromSnapshot(doc));
+}
