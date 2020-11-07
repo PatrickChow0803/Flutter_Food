af3de7d2953e5737705cf246080c3d05bb06d765
patrickchow0803 <patrickchow0803@gmail.com>
2020-11-07 13:21:51 -0500

    Added provider to main.dart and added a ScreensController to determine what screen should be displayed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_food/src/providers/auth.dart';
 import 'package:flutter_food/src/screens/home.dart';
 import 'package:flutter_food/src/screens/login.dart';
+import 'package:flutter_food/src/widgets/loading.dart';
+import 'package:provider/provider.dart';
 
 void main() {
+  // Just for safety reasons.
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
@@ -10,13 +15,37 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Food App',
-        theme: ThemeData(
-          primarySwatch: Colors.red,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: LoginScreen());
+    // Tells the app what providers that I'm going to use throughout the application
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: AuthProvider.initialize()),
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Food App',
+          theme: ThemeData(
+            primarySwatch: Colors.red,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: ScreensController()),
+    );
+  }
+}
+
+class ScreensController extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final auth = Provider.of<AuthProvider>(context);
+    switch (auth.status) {
+      case Status.Uninitialized:
+        return Loading();
+      case Status.Unauthenticated:
+      case Status.Authenticating:
+        return LoginScreen();
+      case Status.Authenticated:
+        return Home();
+      default:
+        return LoginScreen();
+    }
   }
 }
