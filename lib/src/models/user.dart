a050a8cf83f7bc231ed1180ce84e546955481aa4
patrickchow0803 <patrickchow0803@gmail.com>
2020-11-10 20:42:41 -0500

    Modified the user model to use stripe_id

--- lib/src/models/user.dart
+++ lib/src/models/user.dart
@@ -7,20 +7,17 @@ class UserModel {
   static const NAME = 'name';
   static const EMAIL = 'email';
   static const ID = 'id';
-  static const LIKED_FOOD = 'likedFood';
-  static const LIKED_RESTAURANTS = 'likedRestaurants';
+  static const STRIPE_ID = "stripeId";
 
   String _name;
   String _email;
   String _id;
-  List _likedFood;
-  List _likedRestaurants;
+  String _stripeId;
 
   String get name => _name;
   String get email => _email;
   String get id => _id;
-  List get likedFood => _likedFood;
-  List get likedRestaurants => _likedRestaurants;
+  String get stripeId => _stripeId;
 
   // .fromSnapshot can be anything I want. I'm naming it this because that's the data from firebase
 
@@ -28,8 +25,6 @@ class UserModel {
     _name = snapshot.data()[NAME];
     _email = snapshot.data()[EMAIL];
     _id = snapshot.data()[ID];
-    // if snapshot.data()[LIKED_FOOD] is null, set _likedFood's list to an empty list
-    _likedFood = snapshot.data()[LIKED_FOOD] ?? [];
-    _likedRestaurants = snapshot.data()[LIKED_RESTAURANTS] ?? [];
+    _stripeId = snapshot.data()[STRIPE_ID];
   }
 }
